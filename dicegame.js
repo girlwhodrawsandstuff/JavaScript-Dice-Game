29e3a21274068fee39608475cc5c412564f065dc
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-15 23:09:17 +0530

    chore: noted stuff to do

--- dicegame.js
+++ dicegame.js
@@ -5,11 +5,22 @@ function clickedButton(element) {
     // console.log(guessedNumber)
 }
 
+// TODO: handle edge cases
+// - clear guessed number with each round
+// - if use does not enter anything, show a different message
+
+// TODO: 
+// - save all constants like textContent in separate variables
+//   eg: const CORRECT_CHOICE_SELECTED_MESSAGE = 
+// - separate functions, for example updateDiceValue(), renderDice(), renderRoundOutcome(), updateScore(),
+// - Store all things related to timer in separate functions
+
+
 let timeValue = 5
 let score = 0
 
 function startTimer(time) {
-    counter = setInterval(timer, 1000)
+    let counter = setInterval(timer, 1000)
     function timer() {
         let timeCount = document.getElementById("timer")
         timeCount.textContent = time
