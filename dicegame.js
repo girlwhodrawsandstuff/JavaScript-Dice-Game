aaecf0546a56656406d1f991a4dd39d98a18cf6a
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-16 18:18:29 +0530

    refactor: extract functions and change variable names

--- dicegame.js
+++ dicegame.js
@@ -11,29 +11,36 @@ function clickedButton(element) {
     guessedNumber = element.value;
     document.getElementById("guessed-number").textContent = GUESS + guessedNumber;
 }
+
 // - separate functions, for example updateDiceValue(), renderDice(), renderRoundOutcome(), updateScore(),
 // - Store all things related to timer in separate functions
 
 
-let timeValue = 5;
+const seconds = 5;
 let score = 0;
 
-function startTimer(time) {
+function renderTimer(time) {
+    document.getElementById("timer").textContent = time;
+}
+
+function renderDice(diceValue) {
+    const diceImage = "images/dice-" + diceValue + ".png";
+    document.querySelector("img").setAttribute("src", diceImage);
+}
+
+function startTimer(seconds) {
     let counter = setInterval(timer, 1000);
     function timer() {
-        let timeCount = document.getElementById("timer");
-        timeCount.textContent = time;
-        time--;
+        renderTimer(seconds);
 
-        if(time < 0) {
+        if(seconds === 0) {
             clearInterval(counter);
             startTimer(5);
-            let randomNumber = Math.floor(Math.random()*6) + 1;
-            let diceNumber = "images/dice-" + randomNumber + ".png";
-            let image = document.querySelector("img");
-            image.setAttribute("src", diceNumber);
 
-            if(guessedNumber == randomNumber) {
+            let diceValue = Math.floor(Math.random()*6) + 1;
+            renderDice(diceValue);
+
+            if(guessedNumber == diceValue) {
                 score += 1;
                 console.log(score);
                 let finalScore = document.getElementById("score");
@@ -41,7 +48,7 @@ function startTimer(time) {
                 let outcome = document.getElementById("outcome");
                 outcome.textContent = CORRECT_CHOICE_SELECTED_MESSAGE;
                 guessedNumber = 0;
-            } else if(guessedNumber != randomNumber && guessedNumber != 0) {
+            } else if(guessedNumber != diceValue && guessedNumber != 0) {
                 let outcome = document.getElementById("outcome");
                 outcome.textContent = WRONG_CHOICE_SELECTED_MESSAGE;
                 guessedNumber = 0;
@@ -50,7 +57,9 @@ function startTimer(time) {
                 outcome.textContent = NO_CHOICE_SELECTED_MESSAGE;
             }
         }
+
+        seconds--;
     } 
 }
 
-startTimer(timeValue);
+startTimer(seconds);
